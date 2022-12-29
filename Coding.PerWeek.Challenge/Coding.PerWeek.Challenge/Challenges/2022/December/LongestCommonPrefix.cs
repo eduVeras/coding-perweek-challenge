82f207fc19012aecc3ea2273f848f142163123e0
Eduardo Veras <edurveras@hotmail.com>
2022-12-28 22:55:31 -0300

    LongestCommonProfeix

---
+++ Coding.PerWeek.Challenge/Coding.PerWeek.Challenge/Challenges/2022/December/LongestCommonPrefix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding.PerWeek.Challenge.Challenges._2022.December
+{
+    internal class LongestCommonPrefix
+    {
+        public string LongestCommonPrefixCase(string[] strs)
+        {
+
+            if (strs == null || strs.Length == 0)
+            {
+                return "";
+            }
+
+            string first = strs[0];
+            int i = 1;
+            while (i < strs.Length)
+            {
+                while (strs[i].IndexOf(first) != 0)
+                    first = first.Substring(0, first.Length - 1);
+                i++;
+            }
+            return first;
+        }
+
+    }
+}
