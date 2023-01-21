6022baa8635980979688aac1ff1beba4ad3a6efc
Eduardo Veras <edurveras@hotmail.com>
2023-01-21 14:17:28 -0300

    January coding

---
+++ Coding.PerWeek.Challenge/Coding.PerWeek.Challenge/Challenges/2023/January/MajorityElement.cs
@@ -0,0 +1,32 @@
+﻿namespace Coding.PerWeek.Challenge.Challenges._2023.January
+{
+    public class MajorityElement
+    {
+        /// <summary>
+        /// Given an array nums of size n, return the majority element.
+        /// The majority element is the element that appears more than ⌊n / 2⌋ times.You may assume that the majority element always exists in the array.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MajorityElementSolution(int[] nums)
+        {
+
+            var dic = new Dictionary<int,int>();
+
+            var length = nums.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                var val = nums[i];
+                var addSuccess = dic.TryAdd(val,1);
+                if (!addSuccess)
+                {
+                    dic[val] = dic[val] + 1;
+                }                
+            }
+            
+            return dic.MaxBy(kvp => kvp.Value).Key;
+
+        }
+    }
+}
--- Coding.PerWeek.Challenge/Coding.PerWeek.Challenge/Program.cs
+++ Coding.PerWeek.Challenge/Coding.PerWeek.Challenge/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Coding.PerWeek.Challenge.Challenges._2023.January;
+
 Console.WriteLine("Hello, World!");
+new MajorityElement().MajorityElementSolution(new[] { 2, 2, 1, 1, 1, 2, 2 });
