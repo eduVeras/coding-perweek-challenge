15ceecd71da101c8fbb90e4ce3a81cd2cc59be28
Eduardo Veras <edurveras@hotmail.com>
2023-01-25 12:00:26 -0300

    FirstUniqChar

---
+++ Coding.PerWeek.Challenge/Coding.PerWeek.Challenge/Challenges/2023/January/FirstUniqCharProblem.cs
@@ -0,0 +1,26 @@
+﻿namespace Coding.PerWeek.Challenge.Challenges._2023.January
+{
+    internal class FirstUniqCharProblem
+    {
+        public int FirstUniqChar(string s)
+        {
+
+            var dic = new Dictionary<char, Tuple<int,int>>();
+
+            var chars = s.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var addSuccess = dic.TryAdd(chars[i], new Tuple<int, int>(1,i));
+                if (!addSuccess)
+                {
+                    dic[chars[i]] = new Tuple<int, int>(dic[chars[i]].Item1 + 1, dic[chars[i]].Item2);
+                }
+            }
+
+            var result = dic.MinBy(e=> e.Value.Item1).Value;
+
+            return result.Item1 > 1 ? -1 : result.Item2;            
+
+        }
+    }
+}
