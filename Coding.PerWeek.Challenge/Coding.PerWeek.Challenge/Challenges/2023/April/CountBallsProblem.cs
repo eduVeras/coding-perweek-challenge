83acbc3b75e3173fe068ee5a4a7415370c2ec039
Eduardo Veras <edurveras@hotmail.com>
2023-04-09 18:25:29 -0300

    CoutingBalls

---
+++ Coding.PerWeek.Challenge/Coding.PerWeek.Challenge/Challenges/2023/April/CountBallsProblem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding.PerWeek.Challenge.Challenges._2023.April
+{
+    public class CountBallsProblem
+    {
+        public int CountBalls(int lowLimit, int highLimit)
+        {
+
+            var dic = new Dictionary<int, int>();
+            for (int i = lowLimit; i < highLimit +1; i++)
+            {
+                var charArray = i.ToString().ToCharArray();
+                
+                int result = 0;
+                foreach(var num in charArray)
+                {
+                    result += Convert.ToInt32( num.ToString());
+                }
+
+                var addSuccess = dic.TryAdd(result, 1);
+                if (!addSuccess)
+                {
+                    dic[result] = dic[result] + 1;
+                }
+
+            }
+            return dic.MaxBy(kvp => kvp.Value).Value;
+        }
+    }
+}
--- Coding.PerWeek.Challenge/Coding.PerWeek.Challenge/Program.cs
+++ Coding.PerWeek.Challenge/Coding.PerWeek.Challenge/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Coding.PerWeek.Challenge.Challenges._2023.April;
 using Coding.PerWeek.Challenge.Challenges._2023.March;
 
 Console.WriteLine("Hello, World!");
-new SmallerNumbersThanCurrentProblem().SmallerNumbersThanCurrent(new List<int>() { 8, 1, 2, 2, 3 }.ToArray());
+new CountBallsProblem().CountBalls(5, 15);
